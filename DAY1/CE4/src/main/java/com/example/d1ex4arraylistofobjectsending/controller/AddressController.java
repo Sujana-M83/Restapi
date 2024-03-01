ab64006ebf97ee755ec6ce56cc1fd88dd3483262
Sujana-M83 <124856014+Sujana-M83@users.noreply.github.com>
2024-03-01 20:50:02 +0530

    Add files via upload

---
+++ DAY1/CE4/src/main/java/com/example/d1ex4arraylistofobjectsending/controller/AddressController.java
@@ -0,0 +1,23 @@
+package com.example.d1ex4arraylistofobjectsending.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.d1ex4arraylistofobjectsending.module.Address;
+
+import java.util.ArrayList;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class AddressController {
+    @GetMapping("/path")
+    public ArrayList<Address> f1()
+    {
+         ArrayList<Address> d=new ArrayList<Address>();
+         Address l=new Address("hei","abi");
+         d.add(l);
+         return d;
+    }
+    
+}
